aa172351b219b4bcfdc4f162582bdb39aaee5a09
Adryann Felix <adryannofc@gmail.com>
2025-11-24 13:24:52 -0300

    feat: Add initial structure and styles for "Estrelas do Amanhã" and "Tech News" projects
    - Created index.html for "Estrelas do Amanhã" with a registration form.
    - Added CSS modules for form fields, layout, and global styles.
    - Implemented a dropzone for file uploads in the registration form.
    - Established a responsive layout for the "Tech News" project with a header and featured section.
    - Included various SVG icons and images for both projects.
    - Set up utility classes for grid and container layouts.

---
+++ 02-Leetcode/Fundamento/Estrutura de Dados/HashMap.js
@@ -0,0 +1,10 @@
+// O que é HashMap? 🗺️
+// HashMap (ou Map, Dictionary, Hash Table) é uma estrutura de dados que armazena pares chave-valor permitindo acesso super rápido aos dados!
+
+const numerosMap = new Map();
+numerosMap.set(5);
+numerosMap.set(18);
+numerosMap.set(42);
+// ...
+
+console.log(numerosMap.has(42));
